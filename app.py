9a10a9af30506ac746b4c48d98c58f8e1d8485d2
cdaflon <cdaflon@gmail.com.>
2025-10-09 18:15:55 -0300

    db

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///promoprecio.db')
+database_url = os.environ.get('DATABASE_URL', 'sqlite:///promoprecio.db')
+if database_url.startswith('postgres://'):
+    database_url = database_url.replace('postgres://', 'postgresql://', 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
